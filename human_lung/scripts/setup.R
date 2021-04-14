c08ed44ad4234cd8d5c6440b9802cf15ff049521
Andrew Butler <abutler@nygenome.org>
2021-04-13 23:26:33 -0400

    update dims

--- human_lung/scripts/setup.R
+++ human_lung/scripts/setup.R
@@ -11,6 +11,6 @@ meta <- read.csv(file = args[2], row.names = 1)
 ob <- CreateSeuratObject(counts = mat, meta.data = meta)
 ob <- SCTransform(object = ob)
 ob <- RunPCA(object = ob, verbose = FALSE)
-ob <- RunUMAP(object = ob, dims = 1:30, return.model = TRUE)
+ob <- RunUMAP(object = ob, dims = 1:50, return.model = TRUE)
 
 saveRDS(object = ob, file = args[3])
