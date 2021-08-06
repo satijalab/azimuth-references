43cfd04ad747cd49550aff328d9adf8c4aa4fad3
Andrew Butler <andrewwbutler@protonmail.com>
2021-08-06 09:56:37 -0400

    fix demo cells

--- human_kidney/scripts/setup_demo.R
+++ human_kidney/scripts/setup_demo.R
@@ -9,7 +9,7 @@ cells <- read.csv(file = "data/kidney_demo_cells.csv", header = FALSE)
 features <- read.csv(file = "data/kidney_demo_features.csv", header = FALSE)
 meta <- read.csv(file = "data/kidney_demo_metadata.csv", row.names = 1)
 meta <- meta[, c("Project", "Experiment", "celltype", "compartment", "broad_celltype")]
-cells.keep <- read.csv("data/stewart_cells.csv")
+cells.keep <- read.csv("data/stewart_cells.csv", row.names = 1)
 rownames(x = mat) <- cells$V1
 colnames(mat) <- features$V1
 mat <- mat[cells.keep[, 1], ]
