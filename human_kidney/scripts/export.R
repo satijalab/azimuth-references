d6809fe20f5d5f7e3a453264f41e3fbf704b3f20
rsatija <rsatija@nygenome.org>
2021-08-11 17:20:24 -0400

    Immune cell name fixes

--- human_kidney/scripts/export.R
+++ human_kidney/scripts/export.R
@@ -6,6 +6,21 @@ library(Azimuth)
 
 ob <- readRDS(file = args[1])
 
+# small naming changes requested by investigators
+old.names <- c("Monocyte","Tissue-resident Macrophage","Natural Killer / Natural Killer T")
+new.names <- c("Non-classical monocyte", "M2 Macrophage", "Natural Killer T")
+
+l3 <- as.character(ob$annotation.l3)
+l2 <- as.character(ob$annotation.l2)
+
+for(i in 1:3) {
+  l3[l3==old.names[i]] <- new.names[i]
+  l2[l2==old.names[i]] <- new.names[i]
+}
+
+ob$annotation.l2 <- factor(l2,levels = sort(unique(l2)))
+ob$annotation.l3 <- factor(l3,levels = sort(unique(l3)))
+
 ref <- AzimuthReference(
   object = ob, 
   refDR = 'pca',
