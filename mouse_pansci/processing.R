970dd0282626d3fae5ed615332eac73cd172dabc
Gesmira <gesmiramolla@gmail.com>
2024-02-23 16:57:34 -0500

    init pansci

--- mouse_pansci/processing.R
+++ mouse_pansci/processing.R
@@ -1,4 +1,4 @@
-
+# Script to process pansci downsampled dataset
 
 # Convert to seurat object
 # sceasy::convertFormat("/brahms/mollag/azimuth/pansci/20240222_PanSci_all_cells_adata_sampled_by_subtype.h5ad", from="anndata", 
@@ -10,7 +10,8 @@ a <- NormalizeData(a)
 a <- FindVariableFeatures(a, nfeatures = 5000)
 a <- ScaleData(a)
 a <- RunPCA(a, npcs = 100)
-a <- RunUMAP(a, dims = 1:100)
+a <- RunUMAP(a, dims = 1:100, return.model = T)
+saveRDS(a, "/brahms/mollag/azimuth/pansci/pansci_processed_umap_100_dims.rds")
 
 ##### Post processing ####
 a <- readRDS("/brahms/mollag/azimuth/pansci/pansci_processed_umap_100_dims.rds")
@@ -18,32 +19,15 @@ a <- readRDS("/brahms/mollag/azimuth/pansci/pansci_processed_umap_100_dims.rds")
 DimPlot(a, group.by = "Main_cell_type", label = T, repel = T, alpha = 0.1) + NoLegend()
 DimPlot(a, group.by = "Lineage", label = T, repel = T, alpha = 0.1) + NoLegend()
 DimPlot(a, group.by = "Organ_name", label = T, repel = T, alpha = 0.1) + NoLegend()
-saveRDS(obj, full.reference)
 
 
 
-# Build Azimuth Reference
-azimuth.obj <- AzimuthReference(
-  object = obj,
-  refUMAP = "umap",
-  plotref = "umap",
-  refDR = "pca",
-  refAssay = "RNA",
-  metadata = c("celltype.l1", "celltype.l2"),
-  dims = 1:100,
-  reference.version = "2.0.0"
-)
 
 ########### SCTransform VERSION #########
-a <- readRDS("/brahms/mollag/azimuth/pansci/20240222_PanSci_all_cells_adata_sampled_by_subtype.rds")
+b <- readRDS("/brahms/mollag/azimuth/pansci/20240222_PanSci_all_cells_adata_sampled_by_subtype.rds")
 
-a <- SCTransform(a)
-a <- FindVariableFeatures(a, nfeatures = 5000)
-a <- ScaleData(a)
+b <- SCTransform(b, variable.features.n = 5000)
+b <- ScaleData(b)
 a <- RunPCA(a, npcs = 100)
 a <- RunUMAP(a, dims = 1:100)
 
-
-
-# they actually include "sub_umap" coordinates in the metadata? not sure if they computed the umap themselves? 
-# Skylar looked into this and they dont look the best
